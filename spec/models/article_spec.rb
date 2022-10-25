be8f08481ccb9996b69251f6981ee8e443c4ea71
Sean Park <seanpark@Seans-MacBook-Pro.local>
2022-10-25 08:41:56 -0600

    rspec cont

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   it 'tests a number to be positive' do
     expect(1).to be_positive # be_positive => 1.positive? == true
+    expect(5).to be > 3
   end
 end
