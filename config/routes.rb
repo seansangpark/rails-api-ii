b1614bd2718a085963f247438cc7f8730ace1991
Sean Park <seansangpark@gmail.com>
2022-10-26 15:28:30 -0600

    articles details route

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   # get '/articles', to: 'articles#index'
-  resources :articles, only: [:index]
+  resources :articles, only: %i[index show]
 end
