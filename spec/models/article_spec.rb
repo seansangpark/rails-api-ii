6ae19eb0aa5fa5dd26791487ba510e2b319d6b2e
Sean Park <seansangpark@gmail.com>
2022-10-27 07:09:27 -0600

    displaying recent articles first

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -31,10 +31,26 @@ RSpec.describe Article, type: :model do
     it 'validates the uniqueness of the slug' do
       article1 = create(:article)
       expect(article1).to be_valid
-
       article2 = build(:article, slug: article1.slug)
       expect(article2).not_to be_valid
       expect(article2.errors[:slug]).to include('has already been taken')
+    end
+  end
+
+  describe '.recent' do
+    it 'returns articles in the proper order' do
+      older_article = create(:article, created_at: 1.hour.ago)
+      recent_article = create(:article)
+
+      expect(described_class.recent).to eq(
+        [recent_article, older_article]
+      )
+
+      recent_article.update_column(:created_at, 2.hour.ago)
+
+      expect(described_class.recent).to eq(
+        [older_article, recent_article]
+      )
     end
   end
 end
