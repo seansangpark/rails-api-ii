7d8cfd866d2f554aee3120efe4c06edb16e6e5e8
Sean Park <seansangpark@gmail.com>
2022-10-28 09:46:05 -0600

    show action with test cases

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -35,23 +35,45 @@ RSpec.describe ArticlesController do
         eq([recent_article.id, older_article.id])
       )
     end
+
+    it 'paginates results' do
+      article1, article2, article3 = create_list(:article, 3)
+      get '/articles', params: { page: { number: 2, size: 1 } }
+      expect(json_data.length).to eq(1)
+      expect(json_data.first[:id]).to eq(article2.id.to_s)
+    end
+
+    it 'contains pagination links in the response' do
+      article1, article2, article3 = create_list(:article, 3)
+      get '/articles', params: { page: { number: 2, size: 1 } }
+      expect(json[:links].length).to eq(5)
+      expect(json[:links].keys).to contain_exactly(
+        :first, :prev, :next, :last, :self
+      )
+    end
   end
 
-  it 'paginates results' do
-    article1, article2, article3 = create_list(:article, 3)
-    get '/articles', params: { page: { number: 2, size: 1 } }
-    expect(json_data.length).to eq(1)
-    expect(json_data.first[:id]).to eq(article2.id.to_s)
+  describe '#show' do
+    let(:article) { create :article }
+
+    subject { get "/articles/#{article.id}" }
+
+    before { subject }
+
+    it 'returns a success response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a proper JSON' do
+      aggregate_failures do
+        expect(json_data[:id]).to eq(article.id.to_s)
+        expect(json_data[:type]).to eq('articles')
+        expect(json_data[:attributes]).to eq(
+          title: article.title,
+          content: article.content,
+          slug: article.slug
+        )
+      end
+    end
   end
-
-  it 'contains pagination links in the response' do
-    article1, article2, article3 = create_list(:article, 3)
-    get '/articles', params: { page: { number: 2, size: 1 } }
-    expect(json[:links].length).to eq(5)
-    expect(json[:links].keys).to contain_exactly(
-      :first, :prev, :next, :last, :self
-    )
-  end
-
-
 end
