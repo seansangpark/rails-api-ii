b87e36b92a971a8726f33fc2f464ae234d29bb1c
Sean Park <seansangpark@gmail.com>
2022-11-02 17:35:11 -0600

    refactored

--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe UserAuthenticator do
 
       it 'should reuse already registered user' do
         user = create :user, user_data
-        expect { subject }.not_to change { User.count }
+        expect { subject }.not_to (change { User.count })
         expect(authenticator.user).to eq(user)
       end
     end
