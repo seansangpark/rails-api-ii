a76df528818cd03cd6c34b922d8ae88ad7c2d6af
Sean Park <seansangpark@gmail.com>
2022-11-04 10:02:07 -0600

    fixed spelling

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   validates :login, presence: true, uniqueness: true
   validates :provider, presence: true
 
-  has_on :access_token, dependent: :destroy
+  has_one :access_token, dependent: :destroy
 end
