122508ce625d3e3a84ffe6edb6b5a1d00227cd00
Sean Park <seansangpark@gmail.com>
2022-11-06 17:43:57 -0700

    extra lines

--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -5,9 +5,11 @@ require 'rails_helper'
 RSpec.describe AccessToken, type: :model do
   describe '#validations' do
     it 'should have valid factory' do
+
     end
 
     it 'should validate token' do
+      
     end
   end
 
