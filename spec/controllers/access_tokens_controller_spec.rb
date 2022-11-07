ef0f954810b1d1e13b3e1090aa8d91057b990214
Sean Park <seansangpark@gmail.com>
2022-11-07 07:55:19 -0700

    controller spec created

---
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AccessTokensController, type: :controller do
+  describe '#create' do
+    context 'when invalid request' do
+      it 'should return 401 status code' do
+        post :create
+        expect(response).to have_http_status(401)
+      end
+    end
+
+    context 'when success request' do
+    end
+  end
+end
