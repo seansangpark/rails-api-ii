c4facfd946a73f13e760cc84e41c514f9b6905a7
Sean Park <seansangpark@gmail.com>
2022-11-07 16:44:13 -0700

    passing all tests

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -2,5 +2,7 @@ class AccessTokensController < ApplicationController
   def create
     authenticator = UserAuthenticator.new(params[:code])
     authenticator.perform
+
+    render json: {}, status: :created
   end
 end
