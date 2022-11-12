178a76c01a5ce8b1670e88e46b296518e774d5ce
Sean Park <seansangpark@gmail.com>
2022-11-12 08:31:03 -0700

    refactored one

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,4 +1,6 @@
 class AccessTokensController < ApplicationController
+  before_action :authorize!, only: :destroy
+  
   def create
     authenticator = UserAuthenticator.new(params[:code])
     authenticator.perform
@@ -12,12 +14,6 @@ class AccessTokensController < ApplicationController
   end
 
   def destroy
-    provided_token = request.authorization&.gsub(/\ABearer\s/, '')
-    access_token = AccessToken.find_by(token: provided_token)
-    current_user = access_token&.user
-
-    raise AuthorizationError unless current_user
-
     current_user.access_token.destroy
   end
 end
