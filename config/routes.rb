925ce3d17149c152420877197c79927dc4c09e79
Sean Park <seansangpark@gmail.com>
2022-11-13 08:33:15 -0700

    create article endpoint created

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # get '/articles', to: 'articles#index'
   post 'login', to: 'access_tokens#create'
   delete 'logout', to: 'access_tokens#destroy'
-  resources :articles, only: %i[index show]
+
+  resources :articles, only: %i[index show create]
+
 end
